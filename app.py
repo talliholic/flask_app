ae9f9ba083e9f840493f6cb6314fb0a7acfbda50
talliholic <60528222+talliholic@users.noreply.github.com>
2023-02-22 07:46:26 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,30 @@
 from flask import Flask
+from flask_cors import CORS
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host = "151.106.97.0",
+    user = "u679110385_iperez",
+    password = "Kay20071***314",
+    database = "u679110385_eflkids"
+)
 
 app = Flask(__name__)
+app.secret_key = 'super secret key'
+CORS(app)
+
+class User:
+    def __init__(self):
+        self.cursor = mydb.cursor(dictionary=True)
+
+    def read(self, id):
+        self.cursor.execute("SELECT * FROM users WHERE id = %s", (id,))
+        return self.cursor.fetchone()
 
 @app.route("/read", methods=["GET"])
-def read_user():           
-    return "Flask app running"
+def read_user():    
+    user = User()       
+    return user.read(1)
+
+if __name__ == "__main__":
+    app.run(debug=True)
