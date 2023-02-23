df25bb4f1f416d0eec52710a3ea41cc70317dba9
talliholic <60528222+talliholic@users.noreply.github.com>
2023-02-22 20:10:57 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,27 @@
 from flask import Flask
-from flask_cors import CORS
-import mysql.connector
+# from flask_cors import CORS
+# import mysql.connector
 
-mydb = mysql.connector.connect(
-    host = "151.106.97.0",
-    user = "u679110385_iperez",
-    password = "Kay20071***314",
-    database = "u679110385_eflkids"
-)
+# mydb = mysql.connector.connect(
+#     host = "151.106.97.0",
+#     user = "u679110385_iperez",
+#     password = "Kay20071***314",
+#     database = "u679110385_eflkids"
+# )
 
 app = Flask(__name__)
 app.secret_key = 'super secret key'
-CORS(app)
+# CORS(app)
 
-class User:
-    def __init__(self):
-        self.cursor = mydb.cursor(dictionary=True)
+# class User:
+#     def __init__(self):
+#         self.cursor = mydb.cursor(dictionary=True)
 
-    def read(self, id):
-        self.cursor.execute("SELECT * FROM users WHERE id = %s", (id,))
-        return self.cursor.fetchone()
+#     def read(self, id):
+#         self.cursor.execute("SELECT * FROM users WHERE id = %s", (id,))
+#         return self.cursor.fetchone()
 
-@app.route("/read", methods=["GET"])
+@app.route("/", methods=["GET"])
 def read_user():    
-    user = User()       
-    return user.read(1)
+#     user = User()       
+    return "test"
